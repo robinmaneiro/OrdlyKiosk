f8747e80da8e5d4278627366fc3f46b415c3cc99
Robin Maneiro <robin.maneiro@gmail.com>
2025-08-04 18:10:41 +0100

    Implement Koin & Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+    jvmToolchain(17)
     }
     buildFeatures {
         compose = true
@@ -58,5 +58,9 @@ dependencies {
     implementation(libs.lottie.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.coil.compose)
+    //endregion
+
+    //region Dependency injection
+    implementation(libs.koin.compose)
     //endregion
 }
