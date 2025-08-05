0fbea2b75675a7f6df0bc47eb68e3296aa85c13e
Robin Maneiro <robin.maneiro@gmail.com>
2025-08-05 13:20:30 +0100

    Implement Detekt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.detekt)
 }
 
 android {
@@ -63,4 +64,12 @@ dependencies {
     //region Dependency injection
     implementation(libs.koin.compose)
     //endregion
+
+    detektPlugins(libs.compose.detekt.rules)
+}
+
+detekt {
+    autoCorrect = true
+    config.setFrom("../detekt/detekt-config.yml")
+    baseline = file("../detekt/detekt-baseline.yml")
 }
