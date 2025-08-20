015f418825ad6df2edba189024699c7316c3956c
Robin Maneiro <robin.maneiro@gmail.com>
2025-08-20 19:32:49 +0100

    Dashboard composables to separate files

---
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/dashboard/ui/BottomSection.kt
@@ -0,0 +1,55 @@
+package com.robinmaneiro.orderkiosk.dashboard.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.robinmaneiro.orderkiosk.ui.buttons.PrimaryBtnStyle
+import com.robinmaneiro.orderkiosk.ui.buttons.PrimaryButton
+
+@Composable
+fun BottomSection(
+    modifier: Modifier = Modifier
+) {
+    Row (
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxSize()
+    ){
+        PrimaryButton(
+            "Cancel Order",
+            primaryButtonStyle = PrimaryBtnStyle.Red,
+        ) {
+            // Cancel order here
+        }
+
+        // List of orderables here
+        Spacer(
+            modifier = Modifier
+                .background(Color.Green)
+                .padding(310.dp)
+        )
+
+        PrimaryButton(
+            buttonText = "Complete order",
+            primaryButtonStyle = PrimaryBtnStyle.Yellow,
+        ) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BottomSectionPreview() {
+    BottomSection()
+}
