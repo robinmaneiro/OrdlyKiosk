28ff191b089d28095889c6af0bed59b7d7e5c80d
Robin Maneiro <robin.maneiro@gmail.com>
2025-08-20 03:42:15 +0100

    Adding 'description' field to card

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/menu/model/MenuItems.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/menu/model/MenuItems.kt
@@ -1,10 +1,13 @@
 package com.robinmaneiro.orderkiosk.menu.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 class MenuItems: ArrayList<MenuItem>()
 
+@JsonIgnoreProperties(ignoreUnknown = true) // TODO: Change to do this globally
 data class MenuItem(
     @JsonProperty("title") val title: String,
-    @JsonProperty("price") val price: Double
+    @JsonProperty("price") val price: Double,
+    @JsonProperty("description") val description: String
 )
