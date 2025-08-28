12941d32cab03b8ad8bf8abc0d1dc65ac925561f
Robin Maneiro <robin.maneiro@gmail.com>
2025-08-28 20:09:05 +0100

    Define basic network requests

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/networking/RequestManager.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/networking/RequestManager.kt
@@ -6,7 +6,10 @@ import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
+import io.ktor.client.request.patch
+import io.ktor.client.request.post
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.headers
 import io.ktor.serialization.jackson.jackson
@@ -31,13 +34,45 @@ object RequestManager {
 //        addInterceptor(chuckerInterceptor) TODO: Add interceptor
     }
 
+    //region Network Requests
     suspend inline fun <reified T> getRequest(urlString: String): T? {
         val response = httpClient.get(urlString) {
             headers {
-                append(NetworkConstants.Headers.CONTENT_TYPE, NetworkConstants.Values.CONTENT_TYPE )
+                append(NetworkConstants.Headers.CONTENT_TYPE, NetworkConstants.Values.CONTENT_TYPE)
             }
         }
 
         return response.body<T>()
     }
+
+    suspend inline fun <reified T> postRequest(urlString: String): T? {
+        val response = httpClient.post(urlString) {
+            headers {
+                append(NetworkConstants.Headers.CONTENT_TYPE, NetworkConstants.Values.CONTENT_TYPE)
+            }
+        }
+
+        return response.body()
+    }
+
+    suspend inline fun <reified T> patchRequest(urlString: String): T? {
+        val response = httpClient.patch(urlString) {
+            headers {
+                append(NetworkConstants.Headers.CONTENT_TYPE, NetworkConstants.Values.CONTENT_TYPE)
+            }
+        }
+
+        return response.body()
+    }
+
+    suspend inline fun <reified T> deleteRequest(urlString: String): T? {
+        val response = httpClient.delete(urlString) {
+            headers {
+                append(NetworkConstants.Headers.CONTENT_TYPE, NetworkConstants.Values.CONTENT_TYPE)
+            }
+        }
+
+        return response.body()
+    }
+    //endregion
 }
