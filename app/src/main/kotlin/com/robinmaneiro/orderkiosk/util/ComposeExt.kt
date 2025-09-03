3310e296549e61631f72913d4e12d20a5e829765
Robin Maneiro <robin.maneiro@gmail.com>
2025-09-03 18:34:41 +0100

    Adding shadow around bottom price layout

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/util/ComposeExt.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/util/ComposeExt.kt
@@ -1,10 +1,16 @@
 package com.robinmaneiro.orderkiosk.util
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
     clickable(
@@ -14,3 +20,9 @@ fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
         onClick()
     }
 }
+
+fun Modifier.boxShadow() = shadow(
+    elevation = 20.dp,
+    ambientColor = Color(0x80000000),
+    spotColor = Color(0x80000000)
+).background(Color.White, shape = RoundedCornerShape(6.dp))
