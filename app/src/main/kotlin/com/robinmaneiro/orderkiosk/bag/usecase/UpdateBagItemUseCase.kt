8fcc02359d3af187d6ce5e927fb67199e1098edd
Robin Maneiro <robin.maneiro@gmail.com>
2025-09-13 09:59:46 +0100

    Adjust calls for Add and Update bag items

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/usecase/UpdateBagItemUseCase.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/usecase/UpdateBagItemUseCase.kt
@@ -1,9 +1,12 @@
 package com.robinmaneiro.orderkiosk.bag.usecase
 
+import com.robinmaneiro.orderkiosk.bag.model.BagItemResponse
 import com.robinmaneiro.orderkiosk.bag.repository.BagRepository
 
 class UpdateBagItemUseCase(
     private val bagRepository: BagRepository
 ) {
-
+    suspend operator fun invoke(bagItemId: String, newQuantity: Int): BagItemResponse? {
+        return bagRepository.updateBagItem(bagItemId, newQuantity)
+    }
 }
