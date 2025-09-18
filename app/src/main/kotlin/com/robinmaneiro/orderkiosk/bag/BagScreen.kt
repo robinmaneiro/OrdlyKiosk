075a3fa5f4591d8a402f542712602e02eaeed3db
Robin Maneiro <robin.maneiro@gmail.com>
2025-09-18 19:35:48 +0100

    Change design of bag layout

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/BagScreen.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/BagScreen.kt
@@ -1,14 +1,16 @@
 package com.robinmaneiro.orderkiosk.bag
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
@@ -36,16 +38,24 @@ fun BagScreen(
                 .padding(it)
                 .fillMaxSize()
                 .padding(20.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(uiState.bagItems) { bagItemData ->
-                BagItemRow(bagItem = bagItemData,
+            itemsIndexed(uiState.bagItems) { index, bagItemData ->
+                BagItemRow(
+                    bagItem = bagItemData,
                     onPlusClick = { itemId ->
                         viewModel.increaseQuantity(bagItemData)
                     },
                     onMinusClick = { itemId ->
                         viewModel.decreaseQuantity(bagItemData)
-                    })
+                    }
+                )
+                if (index < uiState.bagItems.lastIndex) {
+                    HorizontalDivider(
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                        thickness = 1.dp,
+                        color = Color.Gray
+                    )
+                }
             }
         }
     }
