bd458da3b280954e0642be7cdeaa4ec23cd8da51
Robin Maneiro <robin.maneiro@gmail.com>
2025-09-22 20:48:14 +0100

    Remove container color from top bar

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/ui/SimpleTopBar.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/ui/SimpleTopBar.kt
@@ -1,19 +1,21 @@
 package com.robinmaneiro.orderkiosk.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.robinmaneiro.orderkiosk.ui.theme.Iceberg
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,27 +23,31 @@ fun SimpleTopBar(
     title: String,
     onBack: () -> Unit,
 ) {
-    CenterAlignedTopAppBar(
-        title = {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Text(text = title)
+    Column {
+        TopAppBar(
+            title = {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = title)
+                }
+            },
+            navigationIcon = {
+                IconButton(
+                    onClick = onBack
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
             }
-        },
-        navigationIcon = {
-            IconButton(
-                onClick = onBack
-            ) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                    contentDescription = "Back"
-                )
-            }
-        },
-        colors = TopAppBarDefaults.topAppBarColors().copy(
-            containerColor = Iceberg
         )
-    )
+
+        HorizontalDivider(
+            color = Color.Gray,
+            thickness = 1.dp
+        )
+    }
 }
