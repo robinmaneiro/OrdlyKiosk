67e560dc1911ff86f58f1fa584b14e8f0a4a8533
Robin Maneiro <robin.maneiro@gmail.com>
2025-09-28 20:57:43 +0100

    Define initial functions on LiveContent

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/livecontent/LiveContent.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/livecontent/LiveContent.kt
@@ -6,4 +6,20 @@ object LiveContent {
     suspend fun getString(key: String, default: String): String? {
         return NetworkManager.getRequest<String>("")
     }
+
+    suspend fun getInteger(key: String, default: Int): Int? {
+        return NetworkManager.getRequest<Int>("")
+    }
+
+    suspend fun getFloat(key: String, default: Float): Float? {
+        return NetworkManager.getRequest<Float>("")
+    }
+
+    suspend fun getLong(key: String, default: Long): Long? {
+        return NetworkManager.getRequest<Long>("")
+    }
+
+    suspend inline fun <reified T> getCustomObject(key: String, clazz: Class<T>): T? {
+        return NetworkManager.getRequest<T>("")
+    }
 }
