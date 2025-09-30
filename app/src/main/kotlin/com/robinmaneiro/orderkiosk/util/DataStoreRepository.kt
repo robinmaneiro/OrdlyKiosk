c559713b7e19a19fa5a02ab9daa1c6eb29575066
Robin Maneiro <robin.maneiro@gmail.com>
2025-09-30 08:40:38 +0100

    Basic implementation of Tink

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/util/DataStoreRepository.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/util/DataStoreRepository.kt
@@ -3,22 +3,41 @@ package com.robinmaneiro.orderkiosk.util
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.robinmaneiro.orderkiosk.util.DataStoreRepositoryImpl.Companion.PREFERENCES_NAME
+import kotlinx.coroutines.flow.firstOrNull
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = PREFERENCES_NAME)
 
+private val tokenKey = stringPreferencesKey("token")
+
 interface DataStoreRepository {
-    suspend fun saveToken()
+    suspend fun saveEncryptedToken(token: String)
+    suspend fun getEncryptedToken(): String
 }
 
 class DataStoreRepositoryImpl(
     context: Context
 ) : DataStoreRepository {
     private val dataStore = context.dataStore
+    private suspend fun getDataStore() = dataStore.data.firstOrNull()
 
-    override suspend fun saveToken() {
+    //region Keys
 
+    //endregion
+
+    override suspend fun saveEncryptedToken(token: String) {
+        val encryptedToken = EncryptionUtil.encrypt(token)
+        dataStore.edit {
+            it[tokenKey] = encryptedToken
+        }
+    }
+
+    override suspend fun getEncryptedToken(): String {
+        val encryptedToken = getDataStore()?.get(tokenKey) ?: return ""
+        return EncryptionUtil.decrypt(encryptedToken)
     }
 
     companion object {
