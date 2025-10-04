93227f46b9641e0af23e2978734f2a0411c745dc
Robin Maneiro <robin.maneiro@gmail.com>
2025-10-04 09:46:49 +0100

    Fix issue when updating the Flow<BagResponse?>

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/repository/BagRepositoryImpl.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/repository/BagRepositoryImpl.kt
@@ -13,31 +13,31 @@ class BagRepositoryImpl : BagRepository {
 
     override suspend fun addToBag(productId: String, quantity: Int): Result<BagResponse> {
         val response = NetworkManager.postRequest<BagResponse>("http://192.168.1.162:8080/basket", "{ \"productId\": \"$productId\",\"quantity\": $quantity}")
-        response.onSuccess { _bag.update { it } }
+        response.onSuccess { response -> _bag.update { response } }
         return response
     }
 
     override suspend fun getBagItems(): Result<BagResponse> {
         val response = NetworkManager.getRequest<BagResponse>("http://192.168.1.162:8080/basket")
-        response.onSuccess { _bag.update { it } }
+        response.onSuccess { response -> _bag.update { response } }
         return response
     }
 
     override suspend fun updateBagItem(bagItemId: String, newQuantity: Int): Result<BagResponse> {
         val response = NetworkManager.patchRequest<BagResponse>("http://192.168.1.162:8080/basket/$bagItemId", "{ \"productId\": \"$bagItemId\",\"quantity\": $newQuantity}")
-        response.onSuccess { _bag.update { it } }
+        response.onSuccess { response -> _bag.update { response } }
         return response
     }
 
     override suspend fun removeFromBag(bagItemId: String): Result<BagResponse> {
         val response = NetworkManager.deleteRequest<BagResponse>("http://192.168.1.162:8080/basket/$bagItemId")
-        response.onSuccess { _bag.update { it } }
+        response.onSuccess { response -> _bag.update { response } }
         return response
     }
 
     override suspend fun removeAllBagItems(): Result<BagResponse> {
         val response = NetworkManager.deleteRequest<BagResponse>("http://192.168.1.162:8080/basket/all")
-        response.onSuccess { _bag.update { it } }
+        response.onSuccess { response -> _bag.update { response } }
         return response
     }
 }
