b20a175a15912be929373f9436530a4bbdeb8f39
Robin Maneiro <robin.maneiro@gmail.com>
2025-10-19 08:24:33 +0100

    Refactor network calls' paths

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/account/repository/AccountRepositoryImpl.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/account/repository/AccountRepositoryImpl.kt
@@ -8,23 +8,23 @@ import com.robinmaneiro.orderkiosk.networking.NetworkManager
 
 class AccountRepositoryImpl : AccountRepository {
     override suspend fun registerUser(payload: String): Result<RegistrationResponse> {
-        return NetworkManager.postRequest<RegistrationResponse>("http://192.168.1.162:8080/auth/register", stringBody = payload)
+        return NetworkManager.postRequest<RegistrationResponse>("http://192.168.1.162:8080/api/v1/auth/register", stringBody = payload)
     }
 
     override suspend fun login(payload: String): Result<TokenPairResponse> {
-        return NetworkManager.postRequest<TokenPairResponse>("http://192.168.1.162:8080/auth/login", stringBody = payload)
+        return NetworkManager.postRequest<TokenPairResponse>("http://192.168.1.162:8080/api/v1/auth/login", stringBody = payload)
     }
 
     override suspend fun createGuestSession(): Result<TokenPairResponse> {
-        return NetworkManager.getRequest<TokenPairResponse>("http://192.168.1.162:8080/api/v1/guest-session/create")
+        return NetworkManager.getRequest<TokenPairResponse>("http://192.168.1.162:8080/api/v1/guests/create")
     }
 
     override suspend fun refreshGuestSession(): Result<TokenPairResponse> {
-        return NetworkManager.postRequest<TokenPairResponse>("http://192.168.1.162:8080/api/v1/guest-session/refresh", stringBody = "") // TODO: Add body
+        return NetworkManager.postRequest<TokenPairResponse>("http://192.168.1.162:8080/api/v1/guests/refresh", stringBody = "") // TODO: Add body
     }
 
     override suspend fun getAccountDetails(): Result<AccountDetailsResponse> {
-        return NetworkManager.getRequest("http://192.168.1.162:8080/account/details")
+        return NetworkManager.getRequest("http://192.168.1.162:8080/api/v1/users/me")
     }
 
     override suspend fun getGuestSessionDetails(): Result<GuestSessionDetailsResponse> {
--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/menu/usecase/GetMenuCategoriesUseCase.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/menu/usecase/GetMenuCategoriesUseCase.kt
@@ -1,10 +1,14 @@
 package com.robinmaneiro.orderkiosk.menu.usecase
 
 import com.robinmaneiro.orderkiosk.menu.model.MenuCategories
+import com.robinmaneiro.orderkiosk.menu.repository.MenuRepository
+import com.robinmaneiro.orderkiosk.menu.repository.MenuRepositoryImpl
 import com.robinmaneiro.orderkiosk.networking.NetworkManager
 
-class GetMenuCategoriesUseCase {
+class GetMenuCategoriesUseCase(
+    private val menuRepository: MenuRepository
+) {
     suspend operator fun invoke(): Result<MenuCategories> {
-        return NetworkManager.getRequest<MenuCategories>("http://192.168.1.162:8080/menu/categories") // TODO: Hardcoded string
+        return menuRepository.getAllCategories()
     }
 }
