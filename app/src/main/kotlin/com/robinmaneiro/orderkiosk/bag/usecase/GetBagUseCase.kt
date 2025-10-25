c4fd722cf84cf3c7696955a16b860f0adab1a51b
Robin Maneiro <robin.maneiro@gmail.com>
2025-10-25 21:05:23 +0100

    Adding bagId to GetBagUseCase

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/usecase/GetBagUseCase.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/usecase/GetBagUseCase.kt
@@ -2,11 +2,14 @@ package com.robinmaneiro.orderkiosk.bag.usecase
 
 import com.robinmaneiro.orderkiosk.bag.model.BagResponse
 import com.robinmaneiro.orderkiosk.bag.repository.BagRepository
+import com.robinmaneiro.orderkiosk.datastore.DataStoreRepository
 
 class GetBagUseCase(
-    private val bagRepository: BagRepository
+    private val bagRepository: BagRepository,
+    private val dataStore: DataStoreRepository
 ) {
     suspend operator fun invoke(): Result<BagResponse> {
+        val bagId = dataStore.getAuthBagId().takeIf(String::isNotEmpty) ?: dataStore.getGuestBagId()
         return bagRepository.getBagItems()
     }
 }
