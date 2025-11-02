7fbf8ef6d363719a6e288ee882ec2248ce28cafc
Robin Maneiro <robin.maneiro@gmail.com>
2025-11-02 19:51:30 +0000

    Fix issue with using guest id after user logged in | Retrieve Bag on login

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/account/login/usecase/LoginUserUseCase.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/account/login/usecase/LoginUserUseCase.kt
@@ -3,13 +3,15 @@ package com.robinmaneiro.orderkiosk.account.login.usecase
 import com.robinmaneiro.orderkiosk.account.login.model.LoginPayload
 import com.robinmaneiro.orderkiosk.account.model.TokenPairResponse
 import com.robinmaneiro.orderkiosk.account.repository.AccountRepository
+import com.robinmaneiro.orderkiosk.bag.usecase.GetBagUseCase
 import com.robinmaneiro.orderkiosk.datastore.DataStoreRepository
 import com.robinmaneiro.orderkiosk.util.Mapper
 
 class LoginUserUseCase(
     private val accountRepository: AccountRepository,
     private val dataStore: DataStoreRepository,
-    private val accountDetailsUseCase: AccountDetailsUseCase
+    private val accountDetailsUseCase: AccountDetailsUseCase,
+    private val getBagUseCase: GetBagUseCase
 ) {
     // TODO: What's a better approach for this? Move it to its own AuthUseCase with all of the integrated operations within? And just call other use cases from here?
     suspend operator fun invoke(loginPayload: LoginPayload): Result<TokenPairResponse> {
@@ -17,17 +19,17 @@ class LoginUserUseCase(
 
         return accountRepository.login(payload)
             .onSuccess { loginResponse ->
+                dataStore.saveAuthTokenPair(
+                    accessToken = loginResponse.accessToken,
+                    refreshToken = loginResponse.refreshToken
+                )
                 accountDetailsUseCase.invoke()
                     .onSuccess { accountDetailsResponse ->
-                        dataStore.saveAuthTokenPair(
-                            accessToken = loginResponse.accessToken,
-                            refreshToken = loginResponse.refreshToken
-                        )
-
                         dataStore.saveAccountDetails(accountDetailsResponse)
+                        getBagUseCase.invoke(dataStore.getAuthBagId())
                     }
                     .onFailure {
-                        // TODO: handle account details failure
+                        // TODO: handle account details failure (removing auth tokens too)
                     }
             }
             .onFailure {
