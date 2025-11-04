2597560e180fef0266fdfcbe4c1365ae7b6bfe58
Robin Maneiro <robin.maneiro@gmail.com>
2025-11-04 20:36:39 +0000

    Fix issue when refresh token to auth user

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/account/login/usecase/RefreshTokenUseCase.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/account/login/usecase/RefreshTokenUseCase.kt
@@ -10,7 +10,7 @@ class RefreshTokenUseCase(
     private val dataStore: DataStoreRepository
 ) {
     suspend operator fun invoke() {
-        val payload = Mapper.asSerializedStringResult(RefreshTokenPayload(dataStore.getGuestRefreshToken())).getOrElse { return } // TODO: Better approach for exception?
+        val payload = Mapper.asSerializedStringResult(RefreshTokenPayload(dataStore.getAuthRefreshToken())).getOrElse { return } // TODO: Better approach for exception?
         accountRepository.refreshToken(payload)
             .onSuccess { response ->
                 dataStore.saveAuthTokenPair(
