b50d8abe5de7664d85b2071103e52a5ddcd4a192
Robin Maneiro <robin.maneiro@gmail.com>
2025-12-21 13:22:50 +0000

    Adding image to offers screen

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/offers/OffersScreen.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/offers/OffersScreen.kt
@@ -1,16 +1,36 @@
 package com.robinmaneiro.orderkiosk.offers
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.FontScaling
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
 import com.robinmaneiro.orderkiosk.MainUiEvent
+import com.robinmaneiro.orderkiosk.R
 import com.robinmaneiro.orderkiosk.Screens
 import com.robinmaneiro.orderkiosk.ui.PreviewPixelTablet
+import com.robinmaneiro.orderkiosk.ui.theme.Aquamarine40
 
 @Composable
 fun OffersScreen(
@@ -25,17 +45,39 @@ private fun OffersContent(
     mainUiEvent: (MainUiEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier
+    Column(
+        modifier = Modifier
             .fillMaxSize()
+            .background(Color.DarkGray)
             .clickable {
                 mainUiEvent.invoke(MainUiEvent.NavigateToDestination(Screens.WelcomeScreen.route))
             },
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "This is the OFFERS SCREEN"
+        AsyncImage(
+            modifier = Modifier
+                .size(400.dp, 400.dp)
+                .weight(1f),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(R.drawable.item_test_big_mac)
+                .build(),
+            contentDescription = null
         )
+
+        Row(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(Aquamarine40.copy(alpha = 0.8F)),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                "TOUCH TO START",
+                style = MaterialTheme.typography.labelLarge.copy(color = Color.DarkGray, fontSize = 64.sp),
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }
 
