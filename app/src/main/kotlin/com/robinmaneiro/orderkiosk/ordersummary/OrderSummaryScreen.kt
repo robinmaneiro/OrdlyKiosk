26f567d9d5b90ff69948aad32e734a15ad0ebf7b
Robin Maneiro <robin.maneiro@gmail.com>
2025-12-26 17:42:15 +0000

    order summary draft

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/ordersummary/OrderSummaryScreen.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/ordersummary/OrderSummaryScreen.kt
@@ -1,17 +1,31 @@
 package com.robinmaneiro.orderkiosk.ordersummary
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.robinmaneiro.orderkiosk.NavigationEvent
 import com.robinmaneiro.orderkiosk.ui.PreviewPixelTablet
 import com.robinmaneiro.orderkiosk.ui.SimpleTopBar
@@ -43,8 +57,12 @@ private fun OrderSummaryContent(padding: PaddingValues) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         SectionOrderDetails()
+        Spacer(Modifier.height(10.dp))
         SectionItems()
+        Spacer(Modifier.height(10.dp))
         SectionOrderProgress()
+        Spacer(Modifier.height(10.dp))
+        SectionRoulette()
     }
 }
 
@@ -54,10 +72,12 @@ private fun SectionOrderDetails(
 ) {
     Row {
         Text(
-            "Order Number:"
+            "YOUR ORDER:",
+            style = MaterialTheme.typography.titleLarge.copy(fontSize = 48.sp)
         )
         Text(
-            "12345"
+            "80801",
+            style = MaterialTheme.typography.labelLarge.copy(fontSize = 48.sp)
         )
     }
 }
@@ -66,8 +86,30 @@ private fun SectionOrderDetails(
 private fun SectionItems(
     modifier: Modifier = Modifier
 ) {
-    LazyColumn {
+    val xd = listOf(
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+        "Example of a product",
+
+        )
+    LazyColumn(
+        Modifier.height(200.dp)
+    ) {
         // Just get them from the bag flow
+        items(xd) {
+            Text(it)
+        }
     }
 }
 
@@ -83,14 +125,26 @@ private fun SectionOrderProgress(
         text = "Get Progress of your order "
     )
 
-    Row {
-
-    }
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.Blue)
+    )
 }
 
 @Composable
 private fun SectionRoulette() {
     // Direct to spin the roulette
+    Text(
+        style = MaterialTheme.typography.bodyMedium,
+        text = "Spin the roulette for a chance to get a free meal!"
+    )
+
+    Box(
+        modifier = Modifier
+            .size(50.dp)
+            .background(shape = CircleShape, color = Color.Red)
+    )
 
     // TODO: Move me to new screens - to add some sharing in socials if win
 }
