856d120939287791f91f4ba060cd66e661d6f1a5
Robin Maneiro <robin.maneiro@gmail.com>
2026-01-03 22:20:10 +0000

    Show products in order summary

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/model/BagResponse.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/model/BagResponse.kt
@@ -3,14 +3,17 @@ package com.robinmaneiro.orderkiosk.bag.model
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.robinmaneiro.orderkiosk.util.extensions.getDoublePrice
 import com.robinmaneiro.orderkiosk.util.extensions.getFormattedPrice
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.collections.immutable.toPersistentSet
 
 data class BagResponse(
     @JsonProperty("totalCost") private val _totalCost: PriceData,
-    @JsonProperty("items") val items: List<BagItem>
+    @JsonProperty("items") private val _items: List<BagItem>
 ) {
     val totalCost = _totalCost.withTax
     val formattedTotalCost = totalCost.getFormattedPrice(_totalCost.currencyCode)
-    val itemCount = items.count()
+    val items = _items.toPersistentList()
+    val itemCount = _items.count()
 }
 
 data class BagItem(
