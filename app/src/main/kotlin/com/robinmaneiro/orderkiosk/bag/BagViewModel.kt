b986449b9020d7b7542ac01176b88380873a490b
Robin Maneiro <robin.maneiro@gmail.com>
2026-01-06 23:40:21 +0000

    Redesign bag bottom section

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/BagViewModel.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/bag/BagViewModel.kt
@@ -2,6 +2,8 @@ package com.robinmaneiro.orderkiosk.bag
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 import com.robinmaneiro.orderkiosk.bag.model.BagItem
 import com.robinmaneiro.orderkiosk.bag.model.BagResponse
 import com.robinmaneiro.orderkiosk.bag.model.UpdateBagItemPayload
@@ -155,7 +157,7 @@ class BagViewModel(
     }
 
     data class UiState(
-        val bagItems: List<BagItem> = emptyList(),
+        val bagItems: ImmutableList<BagItem> = persistentListOf(),
         val itemCount: Int = 0,
         val formattedTotalCost: String = "",
         val isLoading: Boolean = false
