6db23e2f9c2c4d8d85b855f651611864e8f230d2
Robin Maneiro <robin.maneiro@gmail.com>
2026-01-06 19:44:17 +0000

    Adding no connection dialog

--- app/src/main/kotlin/com/robinmaneiro/orderkiosk/menu/ui/MenuCategorySection.kt
+++ app/src/main/kotlin/com/robinmaneiro/orderkiosk/menu/ui/MenuCategorySection.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
